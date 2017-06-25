fc379d8e2e2c1350de58e3c25df9d4804f59a486
William Frankish <wfrankish@gmail.com>
2017-06-25 23:01:32 +0100

    finish options page

--- super-comic-buttons/options.js
+++ super-comic-buttons/options.js
@@ -8,21 +8,118 @@ var storage;
 const version = 1.0;
 
 // other variables
-var outOfSync = false;
+var outOfSync;
 const epoch = new Date(0);
 
+// DOM
+var syncStoreRadio;
+var localStoreRadio;
+var noErrorRadio;
+var yesErrorRadio;
+var forceLoadButton;
+var forceSyncButton;
+var forceInfoText;
+
 // initialisation
-$(document).ready(restoreOptions);
+$(document).ready(init);
+
+function init(){
+  outOfSync = false;
+  syncStoreRadio = $("#syncStoreRadio");
+  localStoreRadio = $("#loadStoreRadio");
+  noErrorRadio = $("#noErrorRadio");
+  yesErrorRadio = $("#yesErrorRadio");
+  forceLoadButton = $("#forceLoadButton");
+  forceSaveButton = $("#forceSaveButton");
+  forceInfoText = $("#forceInfoText");
+  restoreOptions();
+}
 
 // load options and restore the page
 function restoreOptions(){
-  /*
-  $("#addAlwaysButton").unbind("click");
-  $("#addAlwaysButton").click(addToAlways);
-  $("#addConditionalButton").unbind("click");
-  $("#addConditionalButton").click(addToConditional);
-  */
+  syncStoreRadio.unbind("change");
+  localStoreRadio.unbind("change");
+  noErrorRadio.unbind("change");
+  yesErrorRadio.unbind("change");
+  forceSaveButton.unbind("click");
+  forceSaveButton.removeClass("glow");
+  forceLoadButton.unbind("click");
+  forceSaveButton.removeClass("glow");
+  forceInfoText.text("Manually Save/Load");
+  forceInfoText.removeClass("warning");
+  var data = loadOptions(false);
+  data.then(_ => {
+    if(useSync){
+      syncStoreRadio.click();
+      localStoreRadio.change(switchSyncOption);
+      forceSaveButton.prop("disabled", false);
+      forceSaveButton.click(forceLoad);
+      forceLoadButton.prop("disabled", false);
+      forceLoadButton.click(forceSave);
+    } else {
+      localStoreRadio.click();
+      syncStoreRadio.change(switchSyncOption);
+      forceSaveButton.prop("disabled", true);
+      forceLoadButton.prop("disabled", true);
+    }
+    if(notifyMe){
+      yesErrorRadio.click();
+      noErrorRadio.change(switchErrorOption);
+    } else {
+      noErrorRadio.click();
+      yesErrorRadio.change(switchErrorOption);
+    }
+    if(outOfSync){
+      forceInfoText.text("Local data and sync data have become out of sync. Please choose to either load sync data or save local data to resolve this");
+      forceInfoText.addClass("warning");
+    }
+  });
 }
+
+function switchSyncOption(){
+  if(useSync){
+    useSync = false;
+    outOfSync = false;
+    var promise = saveOptions(false);
+    promise.then(_ => {
+      restoreOptions();
+    });
+  } else {
+      forceSaveButton.prop("disabled", false);
+      forceLoadButton.click(forceLoad);
+      forceSaveButton.addClass("glow");
+      forceLoadButton.prop("disabled", false);
+      forceSaveButton.click(forceSave);
+      forceLoadButton.addClass("glow");
+      forceInfoText.text("To start using Firefox Sync, please choose whether to load any existing sync data, or save existing local data");
+      forceInfoText.addClass("warning");
+  }
+}
+
+function switchErrorOption(){
+  notifyMe = !notifyMe;
+  var promise = saveOptions(false);
+  promise.then(_ => {
+    restoreOptions();
+  });
+}
+
+function forceLoad(){
+  useSync = true;
+  var promise = loadOptions(true);
+  promise.then(_ => {
+    restoreOptions();
+  });
+}
+
+function forceSave(){
+  useSync = true;
+  var promise = saveOptions(true);
+  promise.then(_ => {
+    restoreOptions();
+  });
+}
+
 
 // load these bits of data, with the following defaults if null
 function loadLocalMetadata(){
@@ -40,10 +137,10 @@ function loadSyncMetadata(){
 }
 
 // save variables to storage
-function saveOptions(force){
+function saveOptions(force = false){
   var now = new Date();
   var metadata = loadLocalMetadata();
-  metadata.then(item => {   
+  var promise = metadata.then(item => {   
     if(useSync){
       saveToSync(force, now, item.lastSaved);
     }
@@ -51,6 +148,7 @@ function saveOptions(force){
     saveToLocal(now);
     lastSaved = now;
   });
+  return promise;
 }
 
 function saveToLocal(now){
@@ -86,13 +184,12 @@ function saveToSync(force, now, lastSavedLocal){
       storage
     });
   }, error => {
-    notifyError("Sync save error", error);
+    notifyError("Sync save error", error.message);
   });
 }
 
-
 // load variables from storage
-function loadOptions(force){
+function loadOptions(force = false){
   var local = loadFromLocal();
   var promise = local.then(_ => {
     if(useSync){
@@ -139,13 +236,14 @@ function loadFromSync(force){
     var loaded = browser.storage.sync.get({
       version : version,
       lastSaved : epoch,
-      storage
+      storage : null
     });
     loaded.then(item => {
+      console.log(item);
       lastSaved = item.lastSaved,
       storage = item.storage;
     });
   }, error => {
-    notifyError("Sync load error", error);
+    notifyError("Sync load error", error.message);
   });
 }
