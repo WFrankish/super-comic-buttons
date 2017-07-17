59d9b746b464298c001cf3d0186231f7c60e85ae
William Frankish <wfrankish@gmail.com>
2017-07-17 20:49:50 +0100

    remove demo functions, meant to do that earlier

--- super-comic-buttons/background.js
+++ super-comic-buttons/background.js
@@ -24,53 +24,6 @@ var reloaded = new Event('reloaded');
 // alarms
 const readAlarm = "readAlarm";
 
-// some rss feeds
-
-// Maybe I'll put some recommendations for webcomics in the wiki
-var demoFeeds = [
-  new Feed({name: "mspa", url: "http://www.mspaintadventures.com/rss/rss.xml", type: "rss"}), // example with lots of items
-  new Feed({name: "oots", url: "http://www.giantitp.com/comics/oots.rss", type: "rss"}), // example with no dates
-  new Feed({name: "megatokyo", url: "http://www.megatokyo.com/rss/", type: "rss"}), // example that currently has bad xml
-  new Feed({name: "zero punctuation", url: "http://www.escapistmagazine.com/rss/videos/list/1.xml", type: "rss"}), // example sometimes with items in the future
-  new Feed({name: "monster pulse", url: "http://www.monster-pulse.com/", id: "cc-comic", overrideLink: "http://www.monster-pulse.com/", type: "html"}), // html example, no root
-  new Feed({name: "nuzlocke", url: "http://nuzlocke.com/pokemonhardmode.php", id: "cg_img", root: "http://nuzlocke.com/", overrideLink: "http://nuzlocke.com/pokemonhardmode.php", type: "html"}) // html example with root
-]
-
-function demo(){
-  storage = new MyArray();
-  demoFeeds.forEach(feed => storage.push(feed));
-  moreDemoFeeds.forEach(url => storage.push(new Feed({name: url, url: url, type: "rss"})));
-  save();
-}
-
-var moreDemoFeeds = [
-"http://paradoxspace.com/rss.atom",
-"http://www.gunnerkrigg.com/rss.xml",
-"http://xkcd.com/rss.xml",
-"http://what-if.xkcd.com/feed.atom",
-"http://drmcninja.com/feed/",
-"http://awkwardzombie.com/awkward.php",
-"http://cucumber.gigidigi.com/feed/",
-"http://penny-arcade.com/feed",
-"http://www.girlgeniusonline.com/ggmain.rss",
-"http://feeds.feedburner.com/AvasDemon?format=xml",
-"http://blindsprings.com/feed",
-"http://mokepon.smackjeeves.com/rss/",
-"http://www.paranatural.net/rss.php",
-"http://www.vibecomic.com/rss.php",
-"http://thepunchlineismachismo.com/feed",
-"http://trenchescomic.com/feed",
-"http://campcomic.com/rss",
-"http://www.beyondthecanopy.com/feed/",
-"http://nedroid.com/feed/",
-"http://feeds.feedburner.com/rsspect/fJur?format=xml",
-"http://www.harkavagrant.com/rssfeed.php",
-"http://sssscomic.com/ssss-feed.xml",
-"http://www.kiwiblitz.com/rss.php",
-"http://badmachinery.com/index.xml",
-"http://www.sleeplessdomain.com/rss.php",
-];
-
 // onload
 $(function(){
   ourUrl = browser.runtime.getURL("");
